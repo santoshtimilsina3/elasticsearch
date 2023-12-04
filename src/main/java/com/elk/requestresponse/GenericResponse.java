79c1ea0b51a82527f9d04e13d1e1500dcb294b64
santosh timilsina <santos.timil@gmail.com>
2023-12-04 14:03:56 +0545

    Resolve-serilization-issue-in-builder-class-error-due-to-inner-class

--- src/main/java/com/elk/requestresponse/GenericResponse.java
+++ src/main/java/com/elk/requestresponse/GenericResponse.java
@@ -1,16 +1,61 @@
 package com.elk.requestresponse;
 
 
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import lombok.Getter;
+
+import java.io.Serializable;
 
 @Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class GenericResponse {
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class GenericResponse implements Serializable {
+    @JsonProperty("StatusCode")
     private int statusCode;
+    @JsonProperty("message")
     private String message;
+    @JsonProperty("data")
     private Object data;
 
+    // Private constructor to be used by the builder
+    private GenericResponse(int statusCode, String message, Object data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static class Builder {
+        @JsonProperty("StatusCode")
+        private int statusCode;
+        @JsonProperty("message")
+        private String message;
+        @JsonProperty("data")
+        private Object data;
+
+        public Builder statusCode(int statusCode) {
+            this.statusCode = statusCode;
+            return this;
+        }
+
+        public Builder message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public Builder data(Object data) {
+            this.data = data;
+            return this;
+        }
+
+        public GenericResponse build() {
+            return new GenericResponse(this.statusCode, this.message, this.data);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
 }
